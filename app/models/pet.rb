56043db553fefabe889dba12765a89db92a89689
Russell Osborne <russell@burningpony.com>
2016-07-27 14:29:54 -0400

    Add last visit on date.

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,10 @@
 class Pet < ActiveRecord::Base
   has_many :visits
+
+  def last_visit_on
+    last_visit.try(:visited_on)
+  end
+  def last_visit
+    visits.order(visited_on: :desc).last
+  end
 end
