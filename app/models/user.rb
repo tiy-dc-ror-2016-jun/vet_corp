48064ef96f3b3e3eb99462a8a933c7d497428250
Russell Osborne <russell@burningpony.com>
2016-07-29 09:41:28 -0400

    Adding signup method to User class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates :email, format: {with: /@/}, uniqueness: true
+
+  def self.sign_up(email)
+    User.create(email: email)
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def test_can_sign_up_user
+    prev_count = User.count
+    User.sign_up("example@example.com")
+
+    assert_equal prev_count + 1, User.count
+  end
+
+  def test_emails_must_be_unique
+    prev_count = User.count
+
+    User.sign_up("example1@example.com")
+    User.sign_up("example1@example.com")
+
+    assert_equal prev_count + 1, User.count
+  end
+
+  def test_cannot_signup_without_a_valid_email
+    prev_count = User.count
+    new_user = User.sign_up("example")
+
+    assert_equal prev_count + 0, User.count
+    refute new_user.valid?
+  end
 end
